9434782b12c8d8957e5b6e808f7df0e0466772c0
stefanenache12 <stefanenache12@gmail.com>
2023-07-13 19:00:02 +0300

    ADD FOOTER SECTION

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -3,24 +3,53 @@ export default {
     name: "FooterComponent",
     data() {
         return {
+            buyComicsImg: [
+                {
+                    img:'src/vue-dc-comics/img/buy-comics-digital-comics.png',
+                    name:'DIGITAL COMICS'
+                },
+                {
+                    img:'src/vue-dc-comics/img/buy-comics-merchandise.png',
+                    name:'DC MERCHANDISE'
+                },
+                {
+                    img:'src/vue-dc-comics/img/buy-comics-shop-locator.png',
+                    name:'SUBSCRIPTION'
+                },
+                {
+                    img:'src/vue-dc-comics/img/buy-comics-subscriptions.png',
+                    name:'COMIC SHOP LOCATOR'
+                },
+                {
+                    img:'src/vue-dc-comics/img/buy-dc-power-visa.svg',
+                    name:'DC POWER VISA'
+                }
+                
+            ],
+
 
         }
     },
     methods: {
-
     }
 }
 </script>
 
 <template>
+    <div class="shop">
+        <div class="container" >
+            <div v-for="singleImg in buyComicsImg" class="img-container">
+                <img :src="singleImg.img" alt="">
+                <h6>{{ singleImg.name }}</h6>
+            </div>
+        </div>
+    </div>
+
     <footer>
         FOOTER
     </footer>
 </template>
 
 <style lang="scss" scoped>
-footer {
-    padding: 40px;
-    text-align: center;
-}
+    @use "src/assets/scss/partials/footer.scss";
 </style>
